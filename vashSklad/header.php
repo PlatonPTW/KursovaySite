3684b0fd5430582c1f3e1d30572f3a3762add247
PlatonPTW <125853654+PlatonPTW@users.noreply.github.com>
2024-03-18 14:35:45 +0300

    Add files via upload

---
+++ vashSklad/header.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-inverse navabar-fixed-top">
+    <div class="navbar-header">
+        <a href="index.php" class="navbar-brand">
+            <img src="/img/Logo1.png" alt="logo">
+        </a>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <?php
+                    if(isset($_SESSION['email'])){
+                ?>
+                    <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+                    <li><a href="settings.php"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
+                    <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                <?php
+                    }else{
+                ?>
+                    <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <?php
+                }
+            ?>
+            </ul>
+        </div>
+</nav>
